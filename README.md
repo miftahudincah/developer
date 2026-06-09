8b285f7474b765e5ebae15b7a45e00e2f659d6ff
miftahudincah <zaki5go@gmail.com>
2026-06-09 15:47:53 +0700

    Hapus semua isi repository

--- README.md
+++
@@ -1,1 +0,0 @@
-sistem absensi web iot 
--- screts.h
+++
@@ -1,9 +0,0 @@
-#ifndef SECRETS_H
-#define SECRETS_H
-
-#define API_KEY "AIzaSyBZg9NpbBAg8dKHkCbYf4J_2bpHH2ZJWWI...."
-#define DATABASE_URL "https://absensi-4389a-default-rtdb.firebaseio.com/"
-
-
-
-#endif
